5d4b998cf41058736321b55d8320b7c0ee404d1f
Angel Restifo <angelrestifo36@gmail.com>
2021-08-04 21:33:30 -0400

    data modeling instagram

--- src/models.py
+++ src/models.py
@@ -26,6 +26,53 @@ class Address(Base):
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
 
+
+class Usuario(Base):
+    __tablename__ = 'usuario'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False)
+    apellido = Column(String(250), nullable=False)
+    correo = Column(String(50), nullable=False)
+    telefono = Column(Integer, nullable=False)
+    sexo = Column(String(10), nullable=False)
+    fecha_nacimiento = Column(, nullable=False)
+    pais = Column(String(20), nullable=False)
+
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, ForeignKey('usuario.id')) 
+    titulo = Column(String(250), nullable=False)
+    imagen = Column(String(250), nullable=False)
+    ubicacion = Column(String(250), nullable=False)
+    comentario_id = Column(Integer, ForeignKey('comentarios.id'))
+    me_gusta_id = Column(Integer, ForeignKey('me_gusta.id')) 
+    
+
+class Comentarios(Base):
+    __tablename__ = 'comentarios'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer)
+    usuario_id = Column(Integer, ForeignKey('usuario.id')) 
+    texto = Column(String(250), nullable=False)
+    
+
+class Mensajes(Base):
+    __tablename__ = 'mensaje'
+    id = Column(Integer, primary_key=True)
+    usuario_id_envia = Column(Integer, ForeignKey('usuario.id')) 
+    usuario_id_recibe = Column(Integer, ForeignKey('usuario.id')) 
+    mensaje = Column(String(250), nullable=False)
+
+class Me_Gusta(Base):
+    __tablename__ = 'me_gusta'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('usuario.id')) 
+    usuario_id = Column(Integer, ForeignKey('usuario.id')) 
+
+
+
     def to_dict(self):
         return {}
 
