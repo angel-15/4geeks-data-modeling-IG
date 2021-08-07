fd3418a50c2c1838c0291df75f1913e98bafd0a3
angel-15 <angelrestifo36@gmail.com>
2021-08-07 15:40:30 +0000

    Data modeling ig

--- src/models.py
+++ src/models.py
@@ -1,30 +1,12 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
 
 Base = declarative_base()
-
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
 
 
 class Usuario(Base):
@@ -33,9 +15,9 @@ class Usuario(Base):
     nombre = Column(String(250), nullable=False)
     apellido = Column(String(250), nullable=False)
     correo = Column(String(50), nullable=False)
-    telefono = Column(Integer, nullable=False)
-    sexo = Column(String(10), nullable=False)
-    fecha_nacimiento = Column(, nullable=False)
+    telefono = Column(Integer)
+    sexo = Column(String(10))
+    fecha_nacimiento = Column(DateTime)
     pais = Column(String(20), nullable=False)
 
 
@@ -43,8 +25,8 @@ class Post(Base):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
     usuario_id = Column(Integer, ForeignKey('usuario.id')) 
-    titulo = Column(String(250), nullable=False)
-    imagen = Column(String(250), nullable=False)
+    titulo = Column(String(250))
+    imagen = Column(String(250))
     ubicacion = Column(String(250), nullable=False)
     comentario_id = Column(Integer, ForeignKey('comentarios.id'))
     me_gusta_id = Column(Integer, ForeignKey('me_gusta.id')) 
